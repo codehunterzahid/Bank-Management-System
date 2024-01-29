eb07581654c6687f503e9501f6a26bed095dd4e5
Zahid Khan <145700090+codehunterzahid@users.noreply.github.com>
2024-01-29 20:17:09 +0500

    Update bank-management-system.cpp

--- bank-management-system.cpp
+++ bank-management-system.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<fstream>
+#include<windows.h>
 #include<cstring>
 #include <unistd.h>
 using namespace std;
 
 //structur for person to store data
     struct person{
-        string name, ID, address,phone;
-		double cash;
+        string name, ID, address,phone,cnic;
+		long int cash;
     };
 
 // A Class is define for Bank 
@@ -15,7 +17,6 @@ class Bank{
 
     int total;
     string id;
-
     person data[10];
 
 
@@ -23,7 +24,10 @@ class Bank{
 
     Bank(){
         total = 0;
+        
+        loadBankData();
     };
+    
 
     void choice();
     void showDetails();
@@ -32,6 +36,9 @@ class Bank{
     void withdraw();
     void deleteAccount();
     void Exit();
+    void saveBankData();
+    void loadBankData();
+    
 };
 
 // Main function is here
@@ -39,6 +46,7 @@ int main(){
 
     Bank b;
     b.choice();
+    b.saveBankData();
     return 0;
 }
 
@@ -78,18 +86,19 @@ void Bank::choice(){
     int ch;
 
     while(1){
-
+    	
+    	Back:
         cout << "\nWelcome To Main Menu " << endl;
         cout << "--------------------" << endl;
 
-        cout << "1. New Account " << endl;
-        cout << "2. Show Details " << endl;
-        cout << "3. Deposit Amount" << endl;
-        cout << "4. Withdraw Amount" << endl;
-        cout << "5. Delete Account" << endl;
+        cout << "1. Create New Account " << endl;
+        cout << "2. Deposit Amount" << endl;
+        cout << "3. Withdraw Amount" << endl;
+        cout << "4. Show Account Details " << endl;
+        cout << "5. Close Account" << endl;
         cout << "6. Exit" << endl;
 
-        Back:
+        
         cout << "Enter Your Choice : ";
         cin >> ch;
 
@@ -101,15 +110,15 @@ void Bank::choice(){
             break;
 
             case 2:
-                Bank::showDetails();
-            break;
-
-            case 3:
                 Bank::deposit();
             break;
 
-            case 4:
+            case 3:
                 Bank::withdraw();
+            break;
+
+            case 4:
+                Bank::showDetails();
             break;
 
             case 5:
@@ -117,11 +126,13 @@ void Bank::choice(){
             break;
 
             case 6:
+            	cout << "Thanks for Choosing Us!"<< endl;
+            	saveBankData();
                 Bank::Exit();
             break;
 
             default:
-                cout << "Invalid Choice" << endl;
+                cout << "Invalid Choice. please Try again." << endl;
                 goto Back;
         }
 
@@ -168,44 +179,63 @@ else if(usrn!=username)
 // Function for new account Creation
 
 void Bank::newAccount() {
+	
+	data[total].cash = 0;
+	
     cout << "Enter Data of Person " << total + 1 << endl;
-    
-    
-    newId:
-    cout << "\nEnter ID : ";
+
+    cout << "\nEnter Account ID : ";
     cin >> data[total].ID;
-    
-    // check if id already exist
+
+    // check if id already exists
     for (int i = 0; i < total; i++) {
         if (data[total].ID == data[i].ID) {
-            cout << "ID already exists. Please use different ID." << endl;
-            goto newId;
+            cout << "Account already exist on this ID." << endl;
             return;
         }
     }
 
-    cout << "Enter Name : ";
-    cin >> data[total].name;
-    
+    cout << "Enter Account Hoder name : ";
+    cin.ignore();
+    getline(cin, data[total].name);
+
     cout << "Enter Address : ";
     cin >> data[total].address;
-    
+
     newPhone:
-    cout << "Enter Phone : ";
+    cout << "Enter 11-Digit Phone Number : ";
     cin >> data[total].phone;
-    
+
     // check if number < 11 digits
-    if (data[total].phone.length() < 11) {
-        cout << "Phone number must have 11 digits." << endl;
+    if (data[total].phone.length() != 11) {
+        cout << "Invalid Phone Number. Please enter an 11-digit number." << endl;
         goto newPhone;
         return;
     }
-    
-    
-    cout << "Enter Cash : ";
-    cin >> data[total].cash;
+
+    newCnic:
+    cout << "Enter Your 13-Digit CNIC Number : ";
+    cin >> data[total].cnic;
+
+    // check if number < 11 digits
+    if (data[total].cnic.length() != 13) {
+        cout << "Invalid CNIC. Please enter a 13-digit CNIC." << endl;
+        goto newCnic;
+        return;
+    }
+
 
     total++;
+    
+    cout << "\nCreating Account";
+    
+    for(int i = 1; i < 4; i++){
+    	cout<<".";
+    	sleep(1);
+	}
+
+    cout<<"\nAccount Created Successfuly";
+    sleep(2);
     
     system("CLS");
 }
@@ -213,13 +243,7 @@ void Bank::newAccount() {
 
 // Function for showing details of all the accounts
 void Bank::showDetails() {
-	
-    if (total == 0) {
-        cout << "Create an account first.\n" << endl;
-        return;
-    }
     
-    again:
     cout << "Enter Id : ";
     cin >> id;
 
@@ -227,17 +251,18 @@ void Bank::showDetails() {
         if (id == data[i].ID) {
         	system("CLS");
         	cout << "Account details of Person " << total << endl;
-        	cout << "\nID : " << data[i].ID << endl;
-            cout << "Name : " << data[i].name << endl;
-            cout << "Address : " << data[i].address << endl;
-            cout << "Cash : " << data[i].cash << endl;
+        	cout << "\nAccount ID : " << data[i].ID << endl;
+            cout << "Account Holder Name : " << data[i].name << endl;
             cout << "Phone : " << data[i].phone << endl;
+            cout << "CNIC :" << data[i].cnic << endl;
+            cout << "Address : " << data[i].address << endl;
+            cout << "Current Balance : " << data[i].cash << endl;
             return;
         }
     }
 
-    cout << "Wrong ID, Try Again." << endl;
-    goto again;
+    cout << "Account not found. Please enter a valid Account ID." << endl;
+    
     
 }
 
@@ -245,82 +270,66 @@ void Bank::showDetails() {
 // Function for depositing amount
 void Bank::deposit() {
     int cash;
-    
-    if (total == 0) {
-        cout << "Create an account first.\n" << endl;
-        return;
-    }
 
-    while (true) {
         cout << "Enter ID : ";
         cin >> id;
 
         for (int i = 0; i < total; i++) {
             if (id == data[i].ID) {
-                cout << "Enter Amount B/W (500-2500): ";
+            	
+            	newCash:
+                cout << "Enter Deposit Amount (Rs 500 - Rs 25000): ";
                 cin >> cash;
-                if (cash < 500) {
-                    cout << "Enter Amount Greater Than 500 : ";
-                    cin >> cash;
+                if (cash < 500 || cash > 25000) {
+                    goto newCash;
                 }
 
                 data[i].cash += cash;
 
-                cout << "Amount Deposited" << endl;
+                cout << "Amount Deposited Successful" << endl;
                 cout << "Account Holder :" << data[i].name << endl;
-                cout << "Your New Cash : " << data[i].cash << endl;
+                cout << "Your New Balance is : " << data[i].cash << endl;
                 return;
             }
         }
 
-        cout << "Invalid ID. Try again." << endl;
-    }
+        cout << "Account not found. Please enter a valid Account ID." << endl;
 }
 
 // Function for withdrawing amount
 void Bank::withdraw() {
     int cash;
     
-    if (total == 0) {
-        cout << "Create an account first.\n" << endl;
-        return;
-    }
 
-    while (true) {
         cout << "Enter ID : ";
         cin >> id;
 
         for (int i = 0; i < total; i++) {
             if (id == data[i].ID) {
                 Amount:
-                cout << "Enter Amount B/W (500-25000): ";
+                cout << "Enter Withdrawal Amount Multiple of 500:(Rs 500 - Rs 25000) : ";
                 cin >> cash;
                 if (cash > data[i].cash) {
-                    cout << "Insufficient Balance" << endl;
-                    cout << "Your Current Balance: " << data[i].cash << endl;
+                    cout << "Insufficient Balance." << endl;
+                    cout << "Your Current Balance is : " << data[i].cash << endl;
                     goto Amount;
-                } else if (cash < 500) {
-                    cout << "Enter Amount Greater Than 500 : ";
-                    goto Amount;
-                } else if (cash > 25000) {
-                    cout << "Enter Amount Less Than 25000 At 1 time : ";
+                } else if (cash < 500 || cash > 25000 || cash % 500 !=0) {
                     goto Amount;
                 } else {
-                    cout << "Withdraw Successful" << endl;
+                    cout << "Withdraw Amount Successfuly" << endl;
                 }
 
                 data[i].cash -= cash;
                 
                 
                 cout << "Account Holder :" << data[i].name << endl;
-                cout << "Your New Cash : " << data[i].cash << endl;
+                cout << "Your New Balance is : " << data[i].cash << endl;
 
                 return;
             }
         }
 
-        cout << "Invalid ID. Try again." << endl;
-    }
+        cout << "Account not found. Please enter a valid Account ID." << endl;
 }
 
 
@@ -328,12 +337,7 @@ void Bank::withdraw() {
 
 void Bank::deleteAccount() {
 	
-	if (total == 0) {
-        cout << "Create an account first.\n" << endl;
-        return;
-    }
 	
-    while (true) {
         cout << "Enter ID : ";
         cin >> id;
 
@@ -344,13 +348,52 @@ void Bank::deleteAccount() {
                 }
                 total--;
 
-                cout << "Account Deleted" << endl;
+                cout << "Account Closed successfuly" << endl;
 
                 return;
             }
         }
 
         cout << "No Such Record Found. Try again." << endl;
+}
+
+
+void Bank::saveBankData() 
+{
+    ofstream file("bank_data.txt");
+    if (file.is_open()) 
+	{
+        file << total << endl;
+        for (int i = 0; i < total; i++) {
+            file << data[i].name << "|" << data[i].ID << "|" << data[i].address << "|" << data[i].phone << "|" << data[i].cnic << "|" << data[i].cash << endl;
+        }
+        cout << "Data saved successfully." << endl;
+    } else {
+        cerr << "Unable to open file for saving bank data.\n";
+    }
+}
+
+
+void Bank::loadBankData() 
+{
+    ifstream file("bank_data.txt");
+    if (file.is_open()) 
+	{
+        file >> total;
+        file.ignore();  // Move to the next line
+        for (int i = 0; i < total; i++) 
+		{
+            getline(file, data[i].name, '|');
+            getline(file, data[i].ID, '|');
+            getline(file, data[i].address, '|');
+            getline(file, data[i].phone, '|');
+            getline(file, data[i].cnic, '|');
+            file >> data[i].cash;
+            file.ignore();  // Move to the next line
+        }
+        file.close();
+    } else {
+        cerr << "Unable to open file for loading bank data.\n";
     }
 }
 
